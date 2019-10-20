ff5af4fe453886d8c1b14d82042802cc81211db8
wchstrife <wch960426@126.com>
2019-10-20 15:11:07 +0800

    Finish 数据预处理

--- clean_data.py
+++ clean_data.py
@@ -4,11 +4,9 @@ import numpy as np
 import time
 import os
 
-import shutil
 import tools
 
-DIRTY_DATA_PATH = './dirty_data'      # 处理前数据路径
-CLEAN_DATA_PATH = './clean_data'    # 处理后数据路径
+CLEAN_DATA_PATH = './clean_data/'    # 处理后数据路径
 
 # 处理account表
 def process_account():
@@ -18,7 +16,7 @@ def process_account():
     account['frequency'].replace({"POPLATEKMESICNE": "POPLATEK MESICNE"}, inplace=True)     # frequency : POPLATEKMESICNE -> POPLATEK MESICNE
 
     # 转换数据类型
-    account['account_id'] = account['account_id'].apply(tools.process_not_int)              
+    account['account_id'] = account['account_id'].apply(tools.account_process_not_int)              
     account['date'] = pd.to_datetime(account['date'])
     
     return account
@@ -42,6 +40,90 @@ def process_client():
 
     return client
 
+# 处理disp表
+def process_disp():
+    disp = tools.read_csv('./dirty_data/disp.csv')
+
+    disp.drop_duplicates(inplace=True)    
+
+    return disp
+
+# 处理district表
+def process_district():
+    district = tools.read_csv('./dirty_data/district.csv')
+
+    district.drop_duplicates(inplace=True)    
+    
+    return district
+
+# 处理loan表
+def process_loan():
+    loan = tools.read_csv('./dirty_data/loan.csv')
+
+    loan['duration'] = loan['duration'].apply(tools.loan_process_duration)     # 处理duration为整12倍数
+    loan = loan[loan['duration'].notnull()]
+    loan = loan[loan.apply(lambda row: tools.loan_process_payment(row['payments'], row['duration'], row['amount']), axis=1)]    # 处理 payment * duration = amount
+
+    return loan
+
+# 处理order表
+def process_order():
+    order = tools.read_csv('./dirty_data/order.csv')
+
+    order.drop_duplicates(inplace=True)  
+
+    return order 
+
+# 处理trans表
+def process_trans():
+    trans = tools.read_csv('./dirty_data/trans.csv')
+
+    trans = trans[trans['balance'].apply(tools.is_int)]     # 处理balance非int
+    trans['balance'] = pd.to_numeric(trans['balance'])
+    trans = trans[trans['bank'].notnull()]                  # 处理bank有空值
+    trans['date'] = pd.to_datetime(trans['date'])           # 格式化date
+
+    return trans
+
+
 if __name__ == "__main__":
-    temp = process_client()
-    print(temp.dtypes)
+    
+    # 读取csv
+    account = process_account()
+    card = process_card()
+    client = process_client()
+    disp = process_disp()
+    district = process_district()
+    loan = process_loan()
+    order = process_order()
+    trans = process_trans()
+
+    # 每一次对表中的外键进行检查
+    # 保存到clean_data目录下
+    account = account[account['district_id'].apply(lambda x: tools.is_exist(x, district, 'district_id'))]  
+    tools.save_csv(CLEAN_DATA_PATH, 'account.csv', account)
+
+    card = card[card['disp_id'].apply(lambda x: tools.is_exist(x, disp, 'disp_id'))]
+    tools.save_csv(CLEAN_DATA_PATH, 'card.csv', card)
+
+    client = client[client['district_id'].apply(lambda x: tools.is_exist(x, district, 'district_id'))]
+    tools.save_csv(CLEAN_DATA_PATH, 'client.csv', client)
+
+    disp = disp[disp['client_id'].apply(lambda x: tools.is_exist(x, client, 'client_id'))]
+    disp = disp[disp['account_id'].apply(lambda x: tools.is_exist(x, account, 'account_id'))]
+    tools.save_csv(CLEAN_DATA_PATH, 'disp.csv', disp)
+
+    tools.save_csv(CLEAN_DATA_PATH, 'district.csv', district)
+
+    loan = loan[loan['account_id'].apply(lambda x: tools.is_exist(x, account, 'account_id'))]
+    tools.save_csv(CLEAN_DATA_PATH, 'loan.csv', loan)
+
+    order = order[order['account_id'].apply(lambda x: tools.is_exist(x, account, 'account_id'))]
+    tools.save_csv(CLEAN_DATA_PATH, 'order.csv', order)
+
+    trans = trans[trans['account_id'].apply(lambda x: tools.is_exist(x, account, 'account_id'))]
+    tools.save_csv(CLEAN_DATA_PATH, 'trans.csv', trans)
+
+
+    
+    
--- tools.py
+++ tools.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
+import time
 
 # 读取CSV
 def read_csv(csv_path):
     return pd.read_csv(csv_path, header=0)
 
+# 保存CSV
+def save_csv(csv_path, filename, table):
+    table.to_csv(csv_path + filename, index=False, sep=',', header=1, encoding='utf_8_sig')
+
 # 处理account表中非int类型数据
-def process_not_int(x):
+def account_process_not_int(x):
         try:
             x = int(x)
             return x
@@ -33,29 +38,26 @@ def not_date(x, format):
     except:
         return True
 
-# 判断日期是否非法
+# 判断日期是否合法
 def is_birth_legal(birth_number):
-    return not is_birth_illegal(birth_number)
-
-def is_birth_illegal(birth_number):
     birth_number = str(birth_number)
     year = 1900 + int(birth_number[0:2])
     try:
         month = int(birth_number[2:4])
     except:
-        return True
+        return False
     day = int(birth_number[4:6])
 
     if month > 12:
         month -= 50
         if month <= 0:
-            return True
+            return False
 
     try:
         time.strptime(str(year) + '-' + str(month) + '-' + str(day), "%Y-%m-%d")
-        return False
-    except:
         return True
+    except:
+        return False
 
 # 处理card表中根据birth分男女
 def card_process_birth(birth_number):
@@ -69,4 +71,32 @@ def card_process_birth(birth_number):
     else:
         sex = 'Male'
     birth_day = pd.datetime(year=year, month=month, day=day)
+
     return pd.Series([birth_day, sex])
+
+# 处理loan表中duration
+# duration应为12的倍数, 否则置位NAN
+def loan_process_duration(x):
+    x = int(x)
+    if x < 0:
+        return np.NaN
+    if x % 12 == 0:
+        return x
+    else:
+        return np.NaN
+
+# 处理loan表中payment
+# payment * duration = Amount
+def loan_process_payment(payment, duration, amount):
+    if payment * duration == amount:
+        return True
+    else:
+        return False
+
+# 判断外键关联
+# 判断table中是否存在该id
+def is_exist(x, table, id_name):
+    if table[(table[id_name] == x)].empty:
+        return False
+    else:
+        return True

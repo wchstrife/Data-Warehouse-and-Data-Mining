3a9b1e16828e80039a8c14dbadf1c174b5b95b9c
wchstrife <wch960426@126.com>
2019-12-14 13:47:17 +0800

    cluster

---
+++ Cluster/main.py
@@ -0,0 +1,15 @@
+#Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('whitegrid')
+
+# %matplotlib inline
+
+dataset = pd.read_csv('./data/data.csv')
+
+dataset.head()
+
+
+
